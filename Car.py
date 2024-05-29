fdafb7d2e0403fc1202e51cf7193a2f0e056bf57
DataKakhadze <datakakhadze@gmail.com>
2024-05-29 16:13:37 +0400

    initial

---
+++ Car.py
@@ -0,0 +1,10 @@
+class Car:
+
+ def __init__(self , color , model):
+     self.color = color
+     self.model = model
+
+
+def drive(self , lola):
+    self.lola = lola
+    print("sound " + lola)
---
+++ main.py
@@ -0,0 +1,9 @@
+from Car import Car
+
+my_car = Car("blue" , "model1")
+
+print(my_car.color + " " + my_car.model)
+
+my_car.drive("lola")
+
+
